4fcc9ded7299ad8857856bb80194303a7ff33995
unknown <dsw43125@student.dsw.edu.pl>
2023-11-14 18:08:17 +0100

    kolejne zmiany test.py pod githuba

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
-import sys
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QPalette, QFont
-from PyQt5.QtCore import Qt
-import unittest
 from unittest.mock import patch, MagicMock
 
 try:
@@ -12,78 +9,83 @@ except ImportError:
 
 from main import MyTestApp
 
-app = QApplication(sys.argv)
-window = MyTestApp(app)
+
+def test_button_colors():
+    app = QApplication([])
+    window = MyTestApp(app)
+    expected_button_color = QPalette().button().color()
+    actual_button_color = window.button_ipv4_info.palette().button().color()
+    assert expected_button_color == actual_button_color
+    app.quit()
 
 
+def test_button_font_size():
+    app = QApplication([])
+    window = MyTestApp(app)
+    expected_font_size = QFont().pointSize()
+    actual_font_size = window.button_ipv4_info.font().pointSize()
+    assert expected_font_size == actual_font_size
+    app.quit()
 
 
-class TestUIElements(unittest.TestCase):
-
-    def test_button_colors(self):
-        expected_button_color = QPalette().button().color()
-        actual_button_color = window.button_ipv4_info.palette().button().color()
-        self.assertEqual(expected_button_color, actual_button_color)
-
-    def test_button_font_size(self):
-        expected_font_size = QFont().pointSize()
-        actual_font_size = window.button_ipv4_info.font().pointSize()
-        self.assertEqual(expected_font_size, actual_font_size)
-
-    def test_text_output_font_size(self):
-        expected_font_size = QFont().pointSize()
-        actual_font_size = window.text_output.font().pointSize()
-        self.assertEqual(expected_font_size, actual_font_size)
-
-    def test_ipv4_info(self):
-        with patch('socket.gethostname', return_value='test_host'):
-            with patch('socket.gethostbyname', return_value='127.0.0.1'):
-                with patch('psutil.net_if_addrs', return_value={'Wi-Fi': [{'family': 2, 'address': '127.0.0.1'}]}):
-                    with patch('socket.gethostbyaddr', return_value=('test_host', [], [])):
-                        app = QApplication([])
-                        window = MyTestApp(app)
-                        window.get_ipv4_info()
-                        text_output = window.text_output.toPlainText()
-                        self.assertIn("IP: 127.0.0.1", text_output)
-                        self.assertIn("Statyczne", text_output)
-                        self.assertIn("Interface: Wi-Fi", text_output)
-                        app.quit()
-
-    def test_system_info(self):
-        with patch('platform.platform', return_value='Windows-10-10.0.19041-SP0'):
-            with patch('platform.architecture', return_value=('32bit', 'WindowsPE')):
-                with patch('psutil.cpu_count', return_value=4):
-                    with patch('psutil.virtual_memory', return_value=psutil._common.svmem(total=4294967296)):
-                        app = QApplication([])
-                        window = MyTestApp(app)
-                        window.get_system_info()
-                        text_output = window.text_output.toPlainText()
-                        self.assertIn("Wersja SO: Windows-10-10.0.19041-SP0", text_output)
-                        self.assertIn("Typ Systemu: ('32bit', 'WindowsPE')", text_output)
-                        self.assertIn("Rdzenie: 4", text_output)
-                        self.assertIn("RAM: 4.0 GB", text_output)
-                        app.quit()
-
-    def test_bios_info(self):
-        with patch('wmi.WMI.Win32_BIOS', return_value=[type('obj', (object,), {'Version': 'test_version'})]):
-            app = QApplication([])
-            window = MyTestApp(app)
-            window.get_bios_info()
-            text_output = window.text_output.toPlainText()
-            self.assertIn("Wersja Biosu: test_version", text_output)
-            app.quit()
-
-    def test_hostname(self):
-        with patch('socket.gethostname', return_value='test_host'):
-            app = QApplication([])
-            window = MyTestApp(app)
-            window.get_hostname()
-            text_output = window.text_output.toPlainText()
-            self.assertIn("Nazwa Hosta: test_host", text_output)
-            app.quit()
+def test_text_output_font_size():
+    app = QApplication([])
+    window = MyTestApp(app)
+    expected_font_size = QFont().pointSize()
+    actual_font_size = window.text_output.font().pointSize()
+    assert expected_font_size == actual_font_size
+    app.quit()
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_ipv4_info():
+    with patch('socket.gethostname', return_value='test_host'):
+        with patch('socket.gethostbyname', return_value='127.0.0.1'):
+            with patch('psutil.net_if_addrs', return_value={'Wi-Fi': [MagicMock(family=2, address='127.0.0.1')]}):
+                with patch('socket.gethostbyaddr', return_value=('test_host', [], [])):
+                    app = QApplication([])
+                    window = MyTestApp(app)
+                    window.get_ipv4_info()
+                    text_output = window.text_output.toPlainText()
+                    assert "IP: 127.0.0.1" in text_output
+                    assert "Statyczne" in text_output
+                    assert "Interface: Wi-Fi" in text_output
+                    app.quit()
+
+
+def test_system_info():
+    with patch('platform.platform', return_value='Windows-10-10.0.19041-SP0'):
+        with patch('platform.architecture', return_value=('32bit', 'WindowsPE')):
+            with patch('psutil.cpu_count', return_value=4):
+                with patch('psutil.virtual_memory', return_value=MagicMock(total=4294967296)):
+                    app = QApplication([])
+                    window = MyTestApp(app)
+                    window.get_system_info()
+                    text_output = window.text_output.toPlainText()
+                    assert "Wersja SO: Windows-10-10.0.19041-SP0" in text_output
+                    assert "Typ Systemu: ('32bit', 'WindowsPE')" in text_output
+                    assert "Rdzenie: 4" in text_output
+                    assert "RAM: 4.0 GB" in text_output
+                    app.quit()
+
+
+def test_bios_info():
+    with patch('wmi.WMI.Win32_BIOS', return_value=[MagicMock(Version='test_version')]):
+        app = QApplication([])
+        window = MyTestApp(app)
+        window.get_bios_info()
+        text_output = window.text_output.toPlainText()
+        assert "Wersja Biosu: test_version" in text_output
+        app.quit()
+
+
+def test_hostname():
+    with patch('socket.gethostname', return_value='test_host'):
+        app = QApplication([])
+        window = MyTestApp(app)
+        window.get_hostname()
+        text_output = window.text_output.toPlainText()
+        assert "Nazwa Hosta: test_host" in text_output
+        app.quit()
+
 
 
